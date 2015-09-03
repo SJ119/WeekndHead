61a9500b944955d80de9588eddb0dbcac9b5721b
Shen Jin <shen_jin@live.com>
2015-09-03 15:39:48 -0400

    Update 2015-09-03T19:39:47.633Z

--- README.md
+++
@@ -1,2 +0,0 @@
-# WeekndHead
-WeekndHead
--- js/weeknd.js
+++ js/weeknd.js
@@ -1,3 +1,19 @@
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID'
+});
+
+$(document).ready(function() {
+  SC.get('/tracks', { ids: '155876731' }, function(tracks) {
+    $(tracks).each(function(index, track) {
+      $('#test').append($('<li></li>').html(track));
+    });
+  });
+});
+
+// var waveform = new Waveform({
+// });
+
+// waveform.data
 var Weeknd = React.createClass({displayName: "Weeknd",
     render: function() {
         return (
